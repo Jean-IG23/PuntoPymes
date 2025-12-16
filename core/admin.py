88c23602d818f1a043c7a26547217da91f032684
MathLeoYa <mateoleonardo1616@gmail.com>
2025-12-16 10:41:31 -0500

    avance

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Empresa, Sucursal, Departamento, Turno, Puesto
+from .models import Empresa, Sucursal, Departamento, Turno, Puesto, Area,   Notificacion
 
 # Registramos los modelos nuevos
 admin.site.register(Empresa)
 admin.site.register(Sucursal)
 admin.site.register(Turno)
+admin.site.register(Area)          # <--- ¡ESTA ES LA QUE FALTABA!
 admin.site.register(Puesto) # <-- Agregamos Puesto en lugar de Rol
 admin.site.register(Departamento)
+admin.site.register(Notificacion)  # <--- Aprovechamos para agregar Notificaciones también
